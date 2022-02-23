512fe2c461098949f5ee689dffe3c0663c6f67fe
Langchao Wu <langchaowu@yahoo.com>
2022-02-23 12:30:10 -0500

    commit tree b9cbedc3dc9c935c473240a11b69beb266981b71

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(drivers){
+    return [drivers[0],drivers[1]];
+}
+function returnLastTwoDrivers(drivers){
+    return [drivers[2],drivers[3]];
+}
+
+const selectingDrivers= [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+
+function createFareMultiplier(num){
+    return function (num2){
+        return num*num2;
+    }
+}
+
+function fareDoubler(num){
+    return num*2;
+}
+
+function fareTripler(num){
+    return num*3;
+}
+
+function selectDifferentDrivers(drivers, func){
+    return func(drivers);
+}
